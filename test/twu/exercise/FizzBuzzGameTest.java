1e6504274a62c557272aab6e7003d62bf1cb61bf
MuchContact <422549477@qq.com>
2015-08-01 14:24:36 +0800

    extract common variables into fields to purify code.

--- test/twu/exercise/FizzBuzzGameTest.java
+++ test/twu/exercise/FizzBuzzGameTest.java
@@ -8,43 +8,37 @@ import static org.junit.Assert.assertEquals;
  * Created by dan on 15-7-31.
  */
 public class FizzBuzzGameTest {
+
+    private final PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
+    private final FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+
     @Test
     public void should_1_print_1() throws Exception {
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
         assertEquals("1", fizzBuzzGame.FizzBuzz(1));
-
     }
 
     @Test
     public void should_divisible_by_3_print_fizz() throws Exception {
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
         assertEquals("Fizz", fizzBuzzGame.FizzBuzz(3));
     }
 
     @Test
     public void should_divisible_by_5_print_buzz() throws Exception {
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
         assertEquals("Buzz", fizzBuzzGame.FizzBuzz(5));
-
     }
 
     @Test
     public void should_divisible_by_3_and_5_print_fizzbuzz() throws Exception {
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
         assertEquals("FizzBuzz", fizzBuzzGame.FizzBuzz(15));
     }
 
     @Test
     public void should_prime_factors_of_1_be_empty() throws Exception {
-        PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
         assertEquals("[]", primeFactorCalculator.generate(1).toString());
-
     }
 
     @Test
     public void should_prime_factors_of_30_be_2_3_5() throws Exception {
-        PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
         assertEquals("[2, 3, 5]", primeFactorCalculator.generate(30).toString());
-
     }
 }
